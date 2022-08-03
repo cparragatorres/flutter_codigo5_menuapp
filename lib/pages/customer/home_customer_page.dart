ff3dd30741606d0bf9f185294f9892785485a774
cparragatorres <humbertpt14@gmail.com>
2022-08-03 00:21:41 -0500

    productos 4

--- lib/pages/customer/home_customer_page.dart
+++ lib/pages/customer/home_customer_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:menuapp/pages/ui/general/colors.dart';
 import 'package:menuapp/pages/ui/widgets/general_widget.dart';
 import 'package:menuapp/pages/ui/widgets/item_category_widget.dart';
+import 'package:menuapp/pages/ui/widgets/item_product_widget.dart';
 import 'package:menuapp/pages/ui/widgets/search_widget.dart';
 import 'package:menuapp/pages/ui/widgets/text_widget.dart';
 
@@ -239,99 +240,7 @@ class _HomeCustomerPageState extends State<HomeCustomerPage> {
                   ),
                 ),
                 divider20,
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 6.0, horizontal: 8.0),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(14.0),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.06),
-                        blurRadius: 12.0,
-                        offset: const Offset(4, 4),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: [
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(14.0),
-                        child: Image.network(
-                          "https://knpr.org/sites/default/files/public/styles/detail_small/public/images/article/octopus_015b.jpg?itok=lWSF96aQ",
-                          height: 120,
-                          width: 120,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      dividerWidth6,
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Pulpo frito",
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                fontWeight: FontWeight.w600,
-                                color: KBrandPrimaryColor,
-                              ),
-                            ),
-                            divider3,
-                            TextNormal(
-                              text: "Los platos de taco, hechos con tortillas de maíz caseras, amplían el cuadro de burrito con opciones adicionales de pescado, barbacoa de res y pulpo estofado.",
-                              color: KBrandPrimaryColor.withOpacity(0.5),
-                              maxLines: 3,
-                            ),
-                            divider3,
-                            Row(
-                              children: [
-                                Row(
-                                  children: List.generate(
-                                    1,
-                                        (index) => Icon(
-                                      Icons.star,
-                                      size: 14.0,
-                                      color: Color(0xffFDBF4F),
-                                    ),
-                                  ),
-                                ),
-                                dividerWidth6,
-                                Text(
-                                  "4.0",
-                                  style: TextStyle(
-                                    fontSize: 12.0,
-                                    color: KBrandPrimaryColor,
-                                  ),
-                                ),
-                                dividerWidth3,
-                                Text("|"),
-                                dividerWidth3,
-                                Text(
-                                  "20 min",
-                                  style: TextStyle(
-                                    fontSize: 12.0,
-                                    color: KBrandPrimaryColor,
-                                  ),
-                                ),
-                                dividerWidth3,
-                                Text("|"),
-                                dividerWidth3,
-                                Text(
-                                  "Porciones: 1",
-                                  style: TextStyle(
-                                    fontSize: 12.0,
-                                    color: KBrandPrimaryColor,
-                                  ),
-                                ),
-                                dividerWidth3,
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                ItemProductWidget(),
               ],
             ),
           ),
